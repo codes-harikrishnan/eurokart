cac3552f573bb9a5a1a741a4c1a9260a514c11b2
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2026-07-14 14:43:19 +0200

    init: Initial Spring boot project setup

---
+++ src/test/java/com/harikrishnan/eurokart/EuroKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harikrishnan.eurokart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroKartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
