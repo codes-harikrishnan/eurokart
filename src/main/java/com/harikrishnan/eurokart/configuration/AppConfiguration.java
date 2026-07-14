a15da56429e0636f425841d1d3f3f2f34f111118
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2026-07-14 16:32:02 +0200

    refactor: remove unused imports in AppConfiguration

--- src/main/java/com/harikrishnan/eurokart/configuration/AppConfiguration.java
+++ src/main/java/com/harikrishnan/eurokart/configuration/AppConfiguration.java
@@ -1,6 +1,5 @@
 package com.harikrishnan.eurokart.configuration;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +9,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.List;
-import java.util.Map;
+
 
 @Configuration
 @RequiredArgsConstructor
