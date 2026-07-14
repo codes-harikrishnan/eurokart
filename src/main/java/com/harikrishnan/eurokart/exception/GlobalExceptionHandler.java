8f50e4ffa92b1522b5b58a6824257fb0fd2f0207
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2026-07-14 16:28:05 +0200

    feat: add HTTPStatus to exceptions

--- src/main/java/com/harikrishnan/eurokart/exception/GlobalExceptionHandler.java
+++ src/main/java/com/harikrishnan/eurokart/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.harikrishnan.eurokart.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleResourceNotFoundException (ResourceNotFoundException exception) {
         return ApiError.builder()
                 .status(HttpStatus.NOT_FOUND.value())
@@ -19,6 +21,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleConfictException (ConflictException exception) {
         return ApiError.builder()
                 .status(HttpStatus.CONFLICT.value())
