acb15c334a8bebe891fa4f068b2d7ecbde83b2f4
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2026-07-15 11:25:01 +0200

    feat: add user entity and associated flyway migration sql

---
+++ src/main/java/com/harikrishnan/eurokart/user/domain/User.java
@@ -0,0 +1,43 @@
+package com.harikrishnan.eurokart.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User (String email, String passwordHash, String role) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+}
